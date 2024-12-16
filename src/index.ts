8ecd421e00f8c30e6b787bc96ec21de406f9aa76
Bram Kortooms <bramkortooms@gmail.com>
2024-12-16 14:26:18 +0200

    fix: test workflow

--- src/index.ts
+++ src/index.ts
@@ -31,10 +31,11 @@ const excludedEntryPoints = getArray<RequestExclusion>('exclude_entry_points');
 const discoveryTypesIn = getArray<Discovery>('discovery_types');
 const hostname = core.getInput('hostname');
 const subdomainsCrawl =
-  core.getInput('sub_domains_crawl', { required: false }) ?? false;
+  core.getBooleanInput('sub_domains_crawl', { required: false }) ?? false;
 const maxInteractionsChainLength =
   parseInt(core.getInput('interactions_depth'), 10) || 3;
-const optimizedCrawler = core.getInput('smart', { required: false }) ?? true;
+const optimizedCrawler =
+  core.getBooleanInput('smart', { required: false }) ?? true;
 const repeaters = getArray('repeaters');
 
 const baseUrl = hostname ? `https://${hostname}` : 'https://app.brightsec.com';
